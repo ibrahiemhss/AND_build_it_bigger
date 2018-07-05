22c66f21457b14f49f472a21093f022ee436068d
ibrahimhss <kekoreko3@gmail.com>
2018-07-06 01:29:12 +0200

    last commit

--- displayjokes/src/main/java/com/example/displayjokes/JokeActivity.java
+++ displayjokes/src/main/java/com/example/displayjokes/JokeActivity.java
@@ -1,41 +1,50 @@
 package com.example.displayjokes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 
 
 public class JokeActivity extends AppCompatActivity {
 
     public static final String JOKE_KEY = "Joke key";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_joke, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5
+                && keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            Log.d("CDA", "onKeyDown Called");
+            onBackPressed();
             return true;
         }
+        return super.onKeyDown(keyCode, event);
+    }
 
-        return super.onOptionsItemSelected(item);
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent myIntent = new Intent(this,Class.forName("com.udacity.gradle.builditbigger.MainActivity"));
+            startActivity(myIntent );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.home || super.onContextItemSelected(item);
+
     }
 }
